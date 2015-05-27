0737c24aef893e0a2ee93748ea45fc6dd880dc42
David Garber & Garrett Olson <student@epicodus.com>
2015-05-27 11:03:23 -0700

    Refactor #convertWord passing all unit tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 var convertWord = function(word) {
   word = word.toLowerCase();
-  var vowels = ["a", "e", "i", "o", "u"];
 
-  if (vowels.indexOf(word.charAt(0))) {
-    word = word + "ay";
+  var endSliceIndex = 0;
+  for(var i = 0; i < word.length; i++) {
+    var char = word[i];
+
+    if (!isVowel(char) ||
+       (word[i - 1] == "q" && char == "u")) {
+      endSliceIndex++;
+    }
+    else if (isVowel(char)) {
+      break;
+    }
   }
+
+  var slicedPortion = word.slice(0, endSliceIndex);
+
+  word = word.replace(slicedPortion, "") + (slicedPortion) + "ay";
+
   return word;
+};
+
+var isVowel = function(char) {
+  return "aeiou".indexOf(char) >= 0;
 };
